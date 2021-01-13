dfd04ba9eaacd2cc8c56fece4f68195cc4f38f3d
jelotz <jelotz@tudelft.nl>
2021-01-13 21:06:38 +0100

    Added documentation

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Scraper in Python to scrape from observado.org
 This script has been tested for python3.
 
 The code can be run by executing: <getobs.py> after editing the 
-link of the overview page of observations to the desired species, date etc.
+link of the overview page of observations to the desired species, date etc. and a name is given.
+
+The resulting .kml file gets the given name and is created in the same folder as the code is executed in.
 
 
 
--- obsclass.py
+++ obsclass.py
@@ -4,10 +4,11 @@ import re
 import simplekml
 import time
 
+
+# Base class containing methods to get soup and set parameters.
 class ScrapeBase:
    def __init__(self, Link):
       self.Link = Link;
-      self.Wait = 0
       self.SetParam()
 
    def SetParam(self):
@@ -18,54 +19,32 @@ class ScrapeBase:
       uClient = uReq(self.Link)
       html = uClient.read()
       uClient.close
-      self.pagesoup = soup(html, "html.parser")
+      self.PageSoup = soup(html, "html.parser")
       print
 
    def PrintSoup(self):
-      print(self.pagesoup)
+      print(self.PageSoup)
 
 
 
+# Class inheriting from baseclass which creates a collection of observations and uses 
+# the class Observation to get its data. Class can be constructed by giving a link 
+# and a name.
 class ObsCollection(ScrapeBase):
 
    def __init__(self, Link, Name):
       self.Link = Link;
       self.Name = Name;
       self.SetParam();
+      
+
+   # Method for performing the whole scraping process after initialization
+   def StartScrapingProcess(self):
       print('\n-------------------------------------')
       print('Starting to scrape new datacollection')
+
+      # Get links to relevant observations
       self.GetObservations();
-
-   def GetObservations(self):
-      self.Obs = [];
-
-      # Create variable in link
-      BaseLink = self.Link[0:-1]
-      page = 0
-
-      print('Starting to retrieve links from overview pages')
-
-      # Get links from page to observations
-      ifend = False
-      while ifend is not True:
-         time.sleep(self.Wait)
-         page +=1
-
-         print('Currently in page %d\r'%page, end="")
-         #print('Currently in page ' + str(page), end="") 
-
-
-         self.Link = BaseLink + str(page)
-         self.GetSoup()
-         for link in self.pagesoup.findAll('a', attrs={'href': re.compile("^/observation/")}):
-            self.Obs.append(link.get('href'))
-         
-         lastpage = self.pagesoup.findAll('li', attrs={'last'})[0].find('a').get('href')
-         if lastpage == None:
-            ifend = True
-            print('Currently in page ' + str(page)) 
-            print("Found " + str(page) + " pages of observations having a total of " + str(len(self.Obs)) + " observations.")
-
 
       # If all links are collected create kml and scrape the pages for the data
       print('Starting extraction of data of observations')
@@ -73,12 +52,48 @@ class ObsCollection(ScrapeBase):
       self.ScrapePages()
       self.SaveKML()
       print("Saved as: " + self.Name)
-
       
+
+   # Method for collecting all the observations from all pages from the given link
+   # This method should be called before ScrapePages()
+   def GetObservations(self):
+      self.Obs = [];
+
+      # Create variable link such that it is able to go through multiple pages
+      BaseLink = self.Link[0:-1]
+      Page = 0
+
+      print('Starting to retrieve links from overview pages')
+
+      # Get links from page to observations
+      IfEnd = False
+      while IfEnd is not True:
+         time.sleep(self.Wait)
+         Page +=1
+
+         print('Currently in page %d\r'%Page, end="")
+         #print('Currently in page ' + str(page), end="") 
+
+
+         self.Link = BaseLink + str(Page)
+         self.GetSoup()
+         for link in self.PageSoup.findAll('a', attrs={'href': re.compile("^/observation/")}):
+            self.Obs.append(link.get('href'))
+         
+         LastPage = self.PageSoup.findAll('li', attrs={'last'})[0].find('a').get('href')
+         if LastPage == None:
+            IfEnd = True
+            print('Currently in page ' + str(Page)) 
+            print("Found " + str(Page) + " pages of observations having a total of " + str(len(self.Obs)) + " observations.")
+
+
+
+   # Method to scrape the collected links to observations from GetObservations(). This
+   # method can only be called after GetObservations()
    def ScrapePages(self):
       i = 0 
-      for link in self.Obs:
-         # Wait to avoid suspicion
+      for iLink in self.Obs:
+         # Optional wait to avoid suspicion
          time.sleep(self.Wait)
 
          i+=1
@@ -88,38 +103,44 @@ class ObsCollection(ScrapeBase):
          else:
             print('Scraping pages [100%]')
 
-         self.LinkObs = link
+         self.LinkObs = iLink
          self.CorrectLinkObs()
-         observation = Observation(self.LinkObs)
-         nogps = observation.GetData();
-         if nogps is False:
-            observation.WriteKMLLine(self.kml)
+         Observation = Observation(self.LinkObs)
+         NoGps = Observation.GetData();
+         # Skip if no gpsdata
+         if NoGps is False:
+            Observation.WriteKMLLine(self.Kml)
 
+   # Method to modify a link to an observation such that is is an actual weblink. Not
+   # an internal link of waarneming.nl.
    def CorrectLinkObs(self):
       self.LinkObs = 'https://waarneming.nl'+self.LinkObs
 
+   # Create .kml file to save observations to.
    def CreateKML(self):
-      self.kml=simplekml.Kml()
+      self.Kml=simplekml.Kml()
 
+   # Saves the created .kml file to self.Name.
    def SaveKML(self):
-      self.kml.save(self.Name, format=True)
+      self.Kml.save(self.Name, format=True)
 
 
+# Class that is used to get the required data of an observation if its link is given. 
+# Can be called seperately to test on a single observation.
+class Observation(ScrapeBase):
 
-
-
-
-class Observation(ScrapeBase):  
-
+   # Method that extracts the data of the webpage of an observation. Can be extended, but
+   # it has to be kept in mind that if new data is added, WriteKMLLine() has to be modified
+   # to get it in the .kml file.
    def GetData(self):
       self.GetSoup();
 
 
-      self.Name = self.pagesoup.find_all("span",{"class": "species-common-name"})[0].text.strip()
+      self.Name = self.PageSoup.find_all("span",{"class": "species-common-name"})[0].text.strip()
       
       # Datum, Aantal, Levensstadium, Activiteit, Locatie, Waarnemer, Protocol, Telmethode, Methode
       # Only date is implemented
-      infotable = self.pagesoup.find_all("table", {"class": "table table-condensed", "id": "observation_details"})
+      infotable = self.PageSoup.find_all("table", {"class": "table table-condensed", "id": "observation_details"})
       info = infotable[0].find_all("td")
       self.DateTime = info[0].text.strip()
       DateTimeList = self.DateTime.replace('-', ' ').split(' ')
@@ -132,7 +153,7 @@ class Observation(ScrapeBase):
          self.Time = '-'
 
       # Location: skip if obscured
-      location = self.pagesoup.find_all("span",{"class": "teramap-coordinates-coords"})
+      location = self.PageSoup.find_all("span",{"class": "teramap-coordinates-coords"})
       if location:
          location = location[0].text.strip().split(", ")
       else:
@@ -144,8 +165,9 @@ class Observation(ScrapeBase):
       return False
 
 
-
-   def WriteKMLLine(self, kml):
-      kml.newpoint(name = self.Name, coords = [(self.Longitude,self.Latitude)])
-#, description='Date: ' + self.DateTime
+   # Method to write the observation data to the .kml file. If new data is required,
+   # it should first be obtained in GetData().
+   def WriteKMLLine(self, Kml):
+      Kml.newpoint(name = self.Name, coords = [(self.Longitude,self.Latitude)])
+      #, description='Date: ' + self.DateTime
 
