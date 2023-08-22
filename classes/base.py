ee837ff3cfa7662c886a492fb98abefd8f59baaa
Jacob Lotz <jacob.lotz@gmail.com>
2023-08-22 19:56:25 +0200

    improved webdriver and clean up of baseclass

--- classes/base.py
+++ classes/base.py
@@ -7,8 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 # Base class containing methods to get soup and set parameters.
 class ScrapeBase:
@@ -23,12 +23,7 @@ class ScrapeBase:
       self.chrome_options.add_argument("--disable-gpu")
       self.chrome_options.add_argument("--no-sandbox") # linux only
       self.chrome_options.add_argument("--headless")
-      #self.chrome_options.page_load_strategy = 'eager'
-      
-      #self.pathdriver = "/home/jelotz/chromedriver"
-      self.pathdriver = "/home/jelotz/prog/chromedriver/chromedriver"
-      
-
+      #self.chrome_options.page_load_strategy = 'eager'    
 
    def GetSoup(self):
       self.browser.get(self.Link)
@@ -38,18 +33,7 @@ class ScrapeBase:
       print(self.PageSoup.prettify)
 
    def CreateWebDriver(self):
-      self.browser = webdriver.Chrome(executable_path = self.pathdriver, chrome_options = self.chrome_options)
+      self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
    def CloseWebDriver(self):
       self.browser.quit()
-
-
-#text_to_be_present_in_element
-
-#EC.element_to_be_clickable((By.CLASS_NAME, "leaflet-clickable")))
-
-
-
-# EC.text_to_be_present_in_element((By.CLASS_NAME, "leaflet-clickable"),"test"))
-
-#EC.text_to_be_present_in_element((By.ID, "operations_monitoring_tab_current_ct_fields_no_data"), "No data to display")
